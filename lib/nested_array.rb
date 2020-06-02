b744ca553080ca1233b879b87571ab574d5cff66
Eve Reichmann <ereichmann4@aol.com>
2020-06-02 15:25:45 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -33,8 +33,7 @@ def sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-matrix = assembled_matrix
-assembled_matrix[1][1]
+matrix[1][1]
 end
 
 def matrix_update(matrix, row, column, new_value)
