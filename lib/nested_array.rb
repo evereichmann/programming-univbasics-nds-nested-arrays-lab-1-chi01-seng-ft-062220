ab986abf595d037487ec5603fd9fdf969ac0985d
Eve Reichmann <ereichmann4@aol.com>
2020-06-02 15:08:31 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,29 +17,23 @@ CONVENTIONAL_PRODUCE = [
 ]
 
 def assembled_matrix
-  grocery_store = [
+  assembled_matrix = [
     CONVENTIONAL_PRODUCE,
     ORGANIC_PRODUCE,
   ]
-  grocery_store
+  assembled_matrix
 end
 
 def sorted_matrix
-  grocery_store = [
+  sorted_matrix = [
     CONVENTIONAL_PRODUCE.sort,
     ORGANIC_PRODUCE.sort,
   ]
-  grocery_store
+  sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Return the matrix's content at that row and and column
-    grocery_store = [
-    CONVENTIONAL_PRODUCE,
-    ORGANIC_PRODUCE,
-  ]
-  grocery_store
+  assembled_matrix[1][1]
 end
 
 def matrix_update(matrix, row, column, new_value)
